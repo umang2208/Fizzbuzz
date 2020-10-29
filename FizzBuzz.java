176a6108442ee2a2d99235762f6525e5bae34804
umang2208 <73518329+umang2208@users.noreply.github.com>
2020-10-29 12:46:37 +0530

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -6,13 +6,17 @@ public class FizzBuzz {
 		// TODO Auto-generated method stub
 		for(int i=1;i<=100;i++) {
 			System.out.println();
+			//first checking number is devisible by 3 and 5 both
 			if(i%3==0&&i%5==0) {
 				System.out.print("FizzBuzz");
 				continue;}
+			//for checking number is divisible by 3
 			if(i%3==0) {
 				System.out.print("Fizz");
 				continue;
 			}
+			//for checking number is divisible by 5
+			
 			if(i%5==0) {
 				System.out.print("Buzz");
 				continue;
